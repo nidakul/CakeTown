6377b62739b2bce4ee4ced3608d29714cd4c2745
nidakul <nidakul@hotmail.com>
2023-08-04 14:18:55 +0300

    Adding About Us

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+
+// Diğer route ve middleware tanımlamaları
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
+
+$(document).ready(function() {
+    $('#navbar-container').load('http://localhost:8000/navbar.html');
+});
+*/
